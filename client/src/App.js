5672983f717890ef784d598075839ac73871f36b
kang <rkdgusdn0319@gmail.com>
2025-02-07 18:18:58 +0900

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import DetailProduct from './components/DetailProduct.jsx';
+import VideoPlayer from './components/VideoPlayer.jsx';
 import './style/common.css';
 
 export default function App() {
   return (
     <div>
-      <DetailProduct />
+      {/* <DetailProduct /> */}
+      <VideoPlayer />
     </div>
   );
 }
