c67e097249012c488890a9f42015e22a381da317
kang <rkdgusdn0319@gmail.com>
2025-02-21 14:12:44 +0900

    commit

--- client/src/components/SignUpPage.jsx
+++ client/src/components/SignUpPage.jsx
@@ -1,6 +1,6 @@
 const inputStyle = "block w-[350px] p-8 mb-12 text-black placeholder-gray-600 bg-white border border-gray-300 rounded-md text-20 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 mx-auto";
-const buttonStyle = "p-8 text-white transition bg-blue-500 rounded-md w-[170px] text-20 hover:bg-blue-700";  // 버튼 너비 175px로 수정
-const buttonContainerStyle = "flex justify-center gap-4 mt-16";  // 버튼 사이 간격을 조정하고, 가운데 정렬
+const buttonStyle = "p-8 text-white transition bg-blue-500 rounded-md w-[170px] text-20 hover:bg-blue-700";  
+const buttonContainerStyle = "flex justify-center gap-4 mt-16";
 
 export default function SignUpPage() {
     return (
